4e94f0349a024a2b1066f5995c986e5eb788fee8
francisco-newton <newton.silvanunes@hotmail.com>
2022-01-24 21:27:57 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,63 @@
-
-import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
 
 function GlobalStyle() {
     return (
-        <style global jsx> {`
-            * {
-                padding: 0;
-                margin: 0;
-                box-size: border-box;
-                list-style: none;
-            }
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-            /* App fit Height */
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #__next {
-                flex: 1;
-            }
-            #__next > * {
-                flex: 1;
-            }
-            /* ./App fit Height */
-        `}</style>
+        <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
     );
 }
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
-
     return (
         <>
             <Tag>{props.children}</Tag>
-
             <style jsx>{`
             ${Tag} {
-                color:${appConfig.theme.colors.neutrals['000']};
-                font-weight: 300;
+                color: ${appConfig.theme.colors.neutrals['000']};
+                font-size: 24px;
+                font-weight: 600;
             }
-            `}
-
-            </style>
+            `}</style>
         </>
-    )
+    );
 }
 
+// Componente React
 // function HomePage() {
+//     // JSX
 //     return (
 //         <div>
 //             <GlobalStyle />
-//             <Titulo tag="h1">Boas vindas de volta!</Titulo>
+//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
 //             <h2>Discord - Alura Matrix</h2>
 //         </div>
 //     )
 // }
-
 // export default HomePage
-
 
 export default function PaginaInicial() {
     const username = 'peas';
